9f3fe52f411d4bc6f2c9d50dad5f2e281b87b504
Yunis Yilmaz <yunis@Yuniss-MacBook-Pro.local>
2024-09-30 23:02:54 -0400

    solve 26. Remove Duplicates from Sorted Array

---
+++ src/bin/problem0026.rs
@@ -0,0 +1,17 @@
+/// Time Complexity: O(n)
+/// Space Complexity: O(1)
+fn remove_duplicates(nums: &mut Vec<i32>) -> i32 {
+    let mut i = 0;
+    for j in 1..nums.len() {
+        if nums[j] != nums[i] {
+            i += 1;
+            nums[i] = nums[j];
+        }
+    }
+    i as i32 + 1
+}
+
+fn main() {
+    assert_eq!(remove_duplicates(&mut vec![1, 1, 2]), 2);
+    assert_eq!(remove_duplicates(&mut vec![0, 0, 1, 1, 1, 2, 2, 3, 3, 4]), 5);
+}
