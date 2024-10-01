49d2b9db1ec00bbd88a68f2554b9d12deb3d57e0
Yunis Yilmaz <yunis@Yuniss-MacBook-Pro.local>
2024-09-30 23:54:36 -0400

    fix range start

--- src/bin/problem0026.rs
+++ src/bin/problem0026.rs
@@ -2,7 +2,7 @@
 /// Space Complexity: O(1)
 fn remove_duplicates(nums: &mut Vec<i32>) -> i32 {
     let mut i = 0;
-    for j in 1..nums.len() {
+    for j in 0..nums.len() {
         if nums[j] != nums[i] {
             i += 1;
             nums[i] = nums[j];
